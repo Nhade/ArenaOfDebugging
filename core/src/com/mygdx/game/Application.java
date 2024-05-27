aa87d22bb603cc7a5d0f31b4f51c6c4733b39684
WeiChanHo <weichan1117@gmail.com>
2024-05-28 02:29:49 +0800

    feat:Create a kinematic body which can move with box2d 's tool

--- core/src/com/mygdx/game/Application.java
+++ core/src/com/mygdx/game/Application.java
@@ -2,9 +2,18 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -15,19 +24,46 @@ import org.json.JSONObject;
 import java.net.URISyntaxException;
 
 public class Application extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Sprite imgSprite;
-	Socket socket;
+	private SpriteBatch batch;
+	private Texture img;
+	private Sprite imgSprite;
+	private World world;
+	private Body body;
+	private Box2DDebugRenderer debugRenderer;
+	private Socket socket;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		imgSprite = new Sprite(img);
 
+		world = new World(new Vector2(0, 0), true);
+		debugRenderer = new Box2DDebugRenderer();
+
+		// Create kinematic body
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.KinematicBody;
+		bodyDef.position.set(400, 240);
+
+		body = world.createBody(bodyDef);
+
+		// Create a circle shape and attach it to the body
+		CircleShape circle = new CircleShape();
+		circle.setRadius(32); // Radius in pixels, scale if necessary
+
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 1f;
+		fixtureDef.friction = 0.5f;
+		fixtureDef.restitution = 0.5f;
+
+		body.createFixture(fixtureDef);
+		circle.dispose();
+
 		// Initialize socket connection
-        try {
-            socket = IO.socket("http://localhost:3000");
+		try {
+			socket = IO.socket("http://localhost:3000");
 
 			socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 				@Override
@@ -46,7 +82,7 @@ public class Application extends ApplicationAdapter {
 				public void call(Object... args) {
 					JSONObject data = (JSONObject) args[0];
 					System.out.println("Game update received: " + data);
-//				handleGameUpdate(data);
+					// handleGameUpdate(data);
 				}
 			}).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
 				@Override
@@ -57,29 +93,64 @@ public class Application extends ApplicationAdapter {
 
 			socket.connect();
 
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@Override
-	public void render () {
+	public void render() {
 		update(Gdx.graphics.getDeltaTime());
 		ScreenUtils.clear(0, 0, 0, 1);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(imgSprite, body.getPosition().x - imgSprite.getWidth() / 2, body.getPosition().y - imgSprite.getHeight() / 2);
 		batch.end();
+
+		debugRenderer.render(world, batch.getProjectionMatrix());
 	}
 
-	private void update(float deltaTime) {
-		// make update to game according to player input, server update
+	private void update(float delta) {
+		handleInput(delta);
+		world.step(delta, 6, 2);
+	}
+
+	private void handleInput(float delta) {
+		float speed = 200;
+		Vector2 velocity = new Vector2();
+		velocity.x = 0;
+		velocity.y = 0;
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			velocity.y += speed * delta;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			velocity.y -= speed * delta;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			velocity.x -= speed * delta;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			velocity.x += speed * delta;
+		}
+
+		// Normalize velocity to ensure consistent speed in all directions
+		if (velocity.len() > 0) {
+			velocity.nor().scl(speed);
+			body.setLinearVelocity(velocity);
+		} else {
+			body.setLinearVelocity(0, 0);
+		}
 	}
 
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
+		world.dispose();
+		debugRenderer.dispose();
+		if (socket != null) {
+			socket.disconnect();
+			socket.close();
+		}
 	}
 }
