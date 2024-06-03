4f153b47293021e80b7c796ce3d26f8407245d12
tomokuh <tomokuh74@gmail.com>
2024-06-03 22:30:06 +0800

    feat: add Obstacle set the coordinates to create the polygon

---
+++ core/src/com/mygdx/game/Obstacle.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Obstacle {
+     Vector2[] vertices;
+
+     Vector2[] vertices1= new Vector2[]{
+        new Vector2(1379,2913),new Vector2(2371,2946),new Vector2(2134,3186),new Vector2(1237,3174)
+     };
+     Vector2[]vertices2= new Vector2[]{
+        new Vector2(3126,1266),new Vector2(2861,1399),new Vector2(3126,2156),new Vector2(2875,2399)
+     };
+     
+     
+}
